087b552f8dbb3d156b89118e6ad2e57065425f69
root <root@berlin.(none)>
2012-02-29 03:59:14 -0800

    tails 1000 lines, scanes, parses, and bans

---
+++ log_monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Scan /var/log/secure for ssh attempts
+# Use iptables to block evil IPs 
+
+echo "Please enter the number of consecutive failed login attempts to block on: "
+read blockAttempts
+
+
+# Reads last 1000 lines from /var/log/secure
+# Check for "Failed password" in SSSH
+# Checks the IP and increments it
+# Loops through the IPs and checks if the same IP
+# Appears more times than the user provide threshold.
+# It then reads the IP and check if it already exists 
+# in the iptables. If it does, it is ignored, otherwise
+# it gets added with a DROP.
+tail -1000 /var/log/secure | awk '/sshd/ && /Failed password for/ { if (/invalid user/) evilIP[$13]++; else evilIP[$11]++; }
+END { for (host in evilIP) if (evilIP[host] > blockAttempts) print host; }' |
+while read ip
+do
+	# 
+	/sbin/iptables -L -n | grep $ip > /dev/null
+	if [ $? -eq 0 ] ; then
+		# echo "already denied ip: [$ip]" ;
+		true
+	else
+		# Block IP
+		/sbin/iptables -A INPUT -s $ip -j DROP
+	fi
+done  
