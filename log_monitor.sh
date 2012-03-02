51c5433df7598001b8ff93f7cc638f5428c7f4f1
Luke Queenan <lukequeenan@gmail.com>
2012-03-02 11:52:17 -0800

    Made functions for everything

--- log_monitor.sh
+++ log_monitor.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-# Make sure the user is running as root (otherwise this won't work)
-if [[ $EUID -ne 0 ]]; then
-    echo "Log Monitor requires root access to run!" 1>&2
-    exit 1
-fi
+
 
 # Scan /var/log/secure for ssh attempts
 # Use iptables to block evil IPs 
 
-echo "Number of failed attempts before block: "
-read blockAttempts
-
-echo "Duration (days?) between first attempt and attempt that causes a block: "
-read monitorDuration
-
-echo "Duration (days?) to block IP for:"
-read blockDuration
 
 # Blocks an IP using iptables and schedules a cronjob for removing the ip after
 # the user defined duration.
@@ -36,18 +24,45 @@ function blockIP()
 # It then reads the IP and check if it already exists 
 # in the iptables. If it does, it is ignored, otherwise
 # it gets added with a DROP.
-tail -1000 /var/log/secure | awk '/sshd/ && /Failed password for/ { if (/invalid user/) evilIP[$13]++; else evilIP[$11]++; }
-END { for (host in evilIP) if (evilIP[host] > blockAttempts) print host; }' |
+function monitor()
+{
+    tail -1000 /var/log/secure | awk 
 
-while read ip
-do
-	# 
-	/sbin/iptables -L -n | grep $ip > /dev/null
-	if [ $? -eq 0 ] ; then
-		# echo "already denied ip: [$ip]" ;
-		true
-	else
-		# Block IP
-		blockIP() $ip
-	fi
-done  
+    '/sshd/ && /Failed password for/ { if (/invalid user/) evilIP[$13]++; else evilIP[$11]++; }
+    END { for (host in evilIP) if (evilIP[host] > blockAttempts) print host; }' |
+
+    while read ip
+    do
+	    # 
+	    /sbin/iptables -L -n | grep $ip > /dev/null
+	    if [ $? -eq 0 ] ; then
+		    # echo "already denied ip: [$ip]" ;
+		    true
+	    else
+		    # Block IP
+		    blockIP $ip
+	    fi
+    done
+}
+
+function main()
+{
+    # Make sure the user is running as root (otherwise this won't work)
+    if [[ $EUID -ne 0 ]]; then
+        echo "Log Monitor requires root access to run!" 1>&2
+        exit 1
+    fi
+
+    echo "Number of failed attempts before block: "
+    read blockAttempts
+
+    echo "Duration (days?) between first attempt and attempt that causes a block: "
+    read monitorDuration
+
+    echo "Duration (days?) to block IP for:"
+    read blockDuration
+    
+    monitor blockAttempts monitorDuration blockDuration
+}
+
+main
