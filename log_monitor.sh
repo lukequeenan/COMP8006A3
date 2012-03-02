eba8a6d6bb9199ed8aed2c6dc31e78314167ed3f
Luke Queenan <lukequeenan@gmail.com>
2012-03-02 11:41:33 -0800

    Added more variables for user input and changed the block ip section to a function.

--- log_monitor.sh
+++ log_monitor.sh
@@ -9,12 +9,27 @@ fi
 # Scan /var/log/secure for ssh attempts
 # Use iptables to block evil IPs 
 
-echo "Please enter the number of consecutive failed login attempts to block on: "
+echo "Number of failed attempts before block: "
 read blockAttempts
+
+echo "Duration (days?) between first attempt and attempt that causes a block: "
+read monitorDuration
+
+echo "Duration (days?) to block IP for:"
+read blockDuration
+
+# Blocks an IP using iptables and schedules a cronjob for removing the ip after
+# the user defined duration.
+function blockIP()
+{
+    /sbin/iptables -A INPUT -s $1 -j DROP
+    
+    #(crontab -l; echo "* * * * * $PWD/unblock_ip.sh") | crontab -
+}
 
 
 # Reads last 1000 lines from /var/log/secure
-# Check for "Failed password" in SSSH
+# Check for "Failed password" in SSHD
 # Checks the IP and increments it
 # Loops through the IPs and checks if the same IP
 # Appears more times than the user provide threshold.
@@ -23,6 +38,7 @@ read blockAttempts
 # it gets added with a DROP.
 tail -1000 /var/log/secure | awk '/sshd/ && /Failed password for/ { if (/invalid user/) evilIP[$13]++; else evilIP[$11]++; }
 END { for (host in evilIP) if (evilIP[host] > blockAttempts) print host; }' |
+
 while read ip
 do
 	# 
@@ -32,6 +48,6 @@ do
 		true
 	else
 		# Block IP
-		/sbin/iptables -A INPUT -s $ip -j DROP
+		blockIP() $ip
 	fi
 done  
